ed61b498e0e79fbee28b0f195661c63b864cabda
Franco Catena <francocatena@gmail.com>
2020-03-12 10:09:44 -0300

    Test config properly formatted

--- config/test.exs
+++ config/test.exs
@@ -2,5 +2,6 @@ import Config
 
 config :psb,
   process_topic: "development-testing-process",
-  publish_topic: "development-testing-process", # They are the same on purpose
+  # They are the same on purpose
+  publish_topic: "development-testing-process",
   project: "k8s-gardinte-development"
